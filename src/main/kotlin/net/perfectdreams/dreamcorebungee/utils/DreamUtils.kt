323f5e9b567379a78da66a9a1a3a0b156b4f7843
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-10-02 22:05:28 -0300

    Adicionar extensions para o TextComponent

--- src/main/kotlin/net/perfectdreams/dreamcorebungee/utils/DreamUtils.kt
+++ src/main/kotlin/net/perfectdreams/dreamcorebungee/utils/DreamUtils.kt
@@ -1,7 +1,9 @@
 package net.perfectdreams.dreamcorebungee.utils
 
 import com.google.gson.JsonParser
+import java.util.*
 
 object DreamUtils {
 	val jsonParser = JsonParser()
+	val random = SplittableRandom()
 }
