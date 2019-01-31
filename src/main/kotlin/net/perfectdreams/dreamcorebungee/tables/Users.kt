1b57183a15319b8b6a48fdd55fe85f4be9bdde13
kaikecarlos <caheasta@gmail.com>
2019-01-30 21:24:35 -0300

    Modificar a tabela para "suportar" o `dao`

--- src/main/kotlin/net/perfectdreams/dreamcorebungee/tables/Users.kt
+++ src/main/kotlin/net/perfectdreams/dreamcorebungee/tables/Users.kt
@@ -1,8 +1,12 @@
 package net.perfectdreams.dreamcorebungee.tables
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IdTable
+import org.jetbrains.exposed.sql.Column
+import java.util.*
 
-object Users : Table() {
-	val id = uuid("id").primaryKey()
-	val username = varchar("username", 16)
+object Users : IdTable<UUID>() {
+	override val id: Column<EntityID<UUID>> = uuid("id").primaryKey().entityId()
+	
+	val username = varchar("username", 16).index()
 }
