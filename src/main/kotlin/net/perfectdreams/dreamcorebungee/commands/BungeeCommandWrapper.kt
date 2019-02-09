d9cf81be07001722269631c3a41d365bd5b8ab83
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-02-09 10:39:16 -0200

    Registrar aliases

--- src/main/kotlin/net/perfectdreams/dreamcorebungee/commands/BungeeCommandWrapper.kt
+++ src/main/kotlin/net/perfectdreams/dreamcorebungee/commands/BungeeCommandWrapper.kt
@@ -6,7 +6,7 @@ import net.md_5.bungee.api.plugin.Command
 class BungeeCommandWrapper(val commandManager: BungeeCommandManager, val sparklyCommand: SparklyBungeeCommand) : Command(
         sparklyCommand.labels.first(), // Label
         "", // Description (nobody cares)
-        "/${sparklyCommand.labels.first()}" // Usage Message (nobody cares²)
+        *sparklyCommand.labels.drop(1).toTypedArray()
 ) {
 
     override fun execute(p0: CommandSender, p1: Array<String>) {
