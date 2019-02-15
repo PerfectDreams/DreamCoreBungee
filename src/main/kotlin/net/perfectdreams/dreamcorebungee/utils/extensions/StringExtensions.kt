ce74d22d4b073317108f9ff48c646a9110afcf09
MrGaabriel <hello@mrgaabriel.space>
2019-02-15 15:12:39 -0200

    Adicionar "UUID#toBytes"

--- src/main/kotlin/net/perfectdreams/dreamcorebungee/utils/extensions/StringExtensions.kt
+++ src/main/kotlin/net/perfectdreams/dreamcorebungee/utils/extensions/StringExtensions.kt
@@ -1,6 +1,16 @@
 package net.perfectdreams.dreamcorebungee.utils.extensions
 
 import net.perfectdreams.dreamcorebungee.utils.TextUtils
+import java.nio.ByteBuffer
+import java.util.*
 
 fun String.centralize() = TextUtils.getCenteredMessage(this)
 fun String.centralizeHeader() = TextUtils.getCenteredHeader(this)
+
+fun UUID.toBytes(): ByteArray {
+    val bb = ByteBuffer.wrap(ByteArray(16))
+    bb.putLong(this.getMostSignificantBits())
+    bb.putLong(this.getLeastSignificantBits())
+
+    return bb.array()
+}
